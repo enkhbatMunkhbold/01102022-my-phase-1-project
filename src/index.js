1420d4e6122f5d3068e30d0e4d25704fcb3b492c
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-03-03 16:35:04 -0800

    Add functionality of displaying movie details

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  getAllMovies()
+  renderFavoriteMovieList()
 })
 
-let getAllMovies = () => {
+let renderFavoriteMovieList = () => {
   fetch('http://localhost:3000/movies') 
   .then(res => res.json()) 
   .then(data => data.forEach(getEveryMovie))
@@ -13,7 +13,17 @@ const makeEl = el => document.createElement(el)
 function getEveryMovie(m) {
   const img = makeEl('img')
   img.src = m.img_link
-  img.className = 'image'
+  img.className = 'poster'
   document.querySelector('div#movie-list').appendChild(img)
-}
 
+  const movie = document.querySelector('div#movie-detail').children
+  const commentsAndRating = document.querySelector('div.commentsAndRating')
+
+  img.addEventListener('click', (e) => { 
+    movie[0].src = e.target.src
+    movie[1].innerText = m.genre
+    movie[2].innerText = m.year
+    commentsAndRating.querySelector('#movie-rating').textContent = m.rating
+    commentsAndRating.querySelector('.movie-comment').textContent = m.comment
+  })
+}
